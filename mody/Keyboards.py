3327f53f0e436070e45d7c47e189a6dd2794053b
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-02-16 19:29:17 +0300

    $

--- mody/Keyboards.py
+++ mody/Keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMa
 from pyrogram.types import ReplyKeyboardRemove
 
 video_url = 'https://graph.org/file/0bdb84af27036dd3561e1.mp4'
-dev = InlineKeyboardButton("丂丨几丂", url=f"https://t.me/D_3_V")
+dev = InlineKeyboardButton("丂丨几丂", url=f"https://t.me/xxStitch")
 
 subs = InlineKeyboardMarkup(
         [
