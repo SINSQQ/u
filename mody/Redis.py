bea8f332dbd34d4f8f958363b52d08561c37cfdf
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-03-14 20:54:41 +0300

    .

--- mody/Redis.py
+++ mody/Redis.py
@@ -1,5 +1,6 @@
 import os
+import ssl
 import redis
 
 redis_url = os.environ.get("REDIS_URL")
-db = redis.StrictRedis.from_url(redis_url,decode_responses=True)
+db = redis.from_url(redis_url, ssl_cert_reqs=ssl.CERT_NONE, decode_responses=True)
