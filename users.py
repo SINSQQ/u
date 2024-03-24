d65ff13580bbb3eaa1930cbf15961ecdffbaf3ab
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-03-25 01:16:54 +0300

    Update users.py

--- users.py
+++ users.py
@@ -283,6 +283,7 @@ async def auto_chat():
         if db.sismember(f'{bot.me.id}:{sudo_info.id}:join_call', userbot.me.id):
             db.srem(f'{bot.me.id}:{sudo_info.id}:join_call', userbot.me.id)
             try:
+                await calls.start()
                 await calls.join_group_call(db.get(f'{bot.me.id}:{sudo_info.id}:call'))
                 await calls.mute_stream(db.get(f'{bot.me.id}:{sudo_info.id}:call'))
             except Exception as e:
@@ -862,7 +863,6 @@ async def start_edited(c, m):
 
 async def main():
     await userbot.start()
-    await calls.start()
     create_task(auto_start_in_bot())
     create_task(auto_gift_in_bot())
     create_task(auto_us_bot())
